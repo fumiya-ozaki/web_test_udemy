1cda0c94c483a20af907ce3337abb5eef153d7e0
fumiya-ozaki <fumiya0414@gmail.com>
2020-04-27 18:26:23 +0900

    オリジナルテンプレートデザイン作成service2

---
+++ page-service2.php
@@ -0,0 +1,26 @@
+<?php
+/*
+Template Name: サービス2
+*/
+?>
+
+<?php get_header(); ?> <!--追加header読み込み-->
+<div id="contents">
+  <div id="conL">
+    <h2><?php the_title(); ?></h2>
+    <p>
+      テストサービスページ
+    </p>
+    <p>
+      テストサービスページ
+    </p>
+    <p>
+      テストサービスページ
+    </p>
+    <p>
+      テストサービスページ
+    </p>
+  </div><!-- /#conL -->
+<?php get_sidebar(); ?>  <!--追加sidebar読み込み-->
+</div><!-- /#contents -->
+<?php get_footer(); 
